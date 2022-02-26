97ac0b240d2fddde144ae248cfb884a462815336
neo-fetch <mayank18529@mechyd.ac.in>
2022-02-26 14:19:46 +0530

    Is there an achievement for this?

---
+++ assignments/solutions/magicSquare.c
@@ -0,0 +1,16 @@
+// coding magic-square problem using OpenMP
+// Compile: gcc -fopenmp magicSquare.c -o magicSquare -lm
+// Run: ./magicSquare
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <omp.h>
+
+
+#define N 100
+#define MAX_THREADS 8
+
+int main(int argc, char *argv[]) {
+    int i, j, k, l, m, n, p, q, r, s, t, u, v, w, x, y, z;
+}    
